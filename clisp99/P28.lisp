6acff1dd1c4374b5d04da95887359b304bf8a080
mariohuang <huangduokai@gmail.com>
2013-06-26 18:44:50 +0800

    add merge-sort P28.lisp

---
+++ clisp99/P28.lisp
@@ -0,0 +1,17 @@
+#!/usr/bin/clisp
+
+(load "P17.lisp")
+(load "merge-sort.lisp")
+
+(defun cmp-func (elea eleb)
+(cond ((and (eql elea nil) (eql eleb nil)) t)
+      ((< (length elea) (length eleb)) t)
+      ((< (length eleb) (length elea)) nil)
+      ((eql (car elea) (car eleb)) (cmp-func (cdr elea) (cdr eleb)))
+      (t)))
+
+(defun lsort (org-list)
+(merge-sort org-list #'cmp-func))
+
+(print (lsort '((a b c) (d e) (f g h) (d e) (i j k l) (m n) (o))))
+
---
+++ clisp99/merge-sort.lisp
@@ -0,0 +1,19 @@
+#!/usr/bin/clisp
+
+(defun merge-list (lista listb cmp-func)
+(cond ((eql lista nil) listb)
+      ((eql listb nil) lista)
+      ((funcall cmp-func (car lista) (car listb))
+       (append (list (car lista)) (merge-list (cdr lista) listb cmp-func)) )
+      (t (append (list (car listb)) (merge-list lista (cdr listb) cmp-func)))
+      )
+)
+      
+(defun merge-sort (org-list cmp-func)
+(cond ((eql org-list nil) nil)
+      ((eql (length org-list) 1) org-list)
+      (t (let ( (split-res (split org-list (floor (/ (+ 1 (length org-list)) 2)))) ) 
+              (merge-list (merge-sort (car split-res) cmp-func) (merge-sort (cadr split-res) cmp-func) cmp-func)))
+      )
+)
+
